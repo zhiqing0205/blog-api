990b02b57eaf0943d5bc34cb8651b6b300200e65
Zhiqing ZHong <1775840762@qq.com>
2021-11-06 01:05:01 +0800

    2-9 集成热部署    compiler.automake.allow.when.app.running在新版本位置改变    在setting->Advanced Setttings里面的第一个设置勾上就行了    Unable to start LiveReload server 因为端口被占用了    在配置文件中找一个闲置端口配置上即可

--- src/main/java/com/ziuch/blog/api/controller/TestController.java
+++ src/main/java/com/ziuch/blog/api/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ziuch.blog.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/testApi")
 public class TestController {
 
+    @Value("${test.hello:defaultValue}")
+    private String helloWorld;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello world";
+        return "Hello world " + helloWorld;
     }
 
     @PostMapping("/hello/post")
