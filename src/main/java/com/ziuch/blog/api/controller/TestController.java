978ffbe86a6a031698b2589be6939b09dff33ca3
Zhiqing ZHong <1775840762@qq.com>
2021-11-05 23:59:15 +0800

    2-6 开发hello world接口 访问地址：http://127.0.0.1:9000/hello

---
+++ src/main/java/com/ziuch/blog/api/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.ziuch.blog.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+}
