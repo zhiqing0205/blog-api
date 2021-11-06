c7c6a8b2476a4c6e5a71d30f24ea00c1f0775aec
Zhiqing ZHong <1775840762@qq.com>
2021-11-07 00:30:30 +0800

    解决跨域问题

---
+++ src/main/java/com/ziuch/blog/api/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.ziuch.blog.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域问题解决
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
