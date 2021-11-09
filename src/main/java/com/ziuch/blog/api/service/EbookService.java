ff516e84d7c34d52c4da670cec12bbcbf892c36b
Zhiqing Zhong <1775840762@qq.com>
2021-11-09 22:36:00 +0800

    增加PageHelper插件 为EbookService加入分页 配置日志输出数据库执行语句

--- src/main/java/com/ziuch/blog/api/service/EbookService.java
+++ src/main/java/com/ziuch/blog/api/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.ziuch.blog.api.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.ziuch.blog.api.domain.Ebook;
 import com.ziuch.blog.api.domain.EbookExample;
 import com.ziuch.blog.api.mapper.EbookMapper;
 import com.ziuch.blog.api.req.EbookReq;
 import com.ziuch.blog.api.resp.EbookResp;
 import com.ziuch.blog.api.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -18,14 +22,23 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     public List<EbookResp> list(EbookReq req){
+
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
 
         if(!ObjectUtils.isEmpty(req.getName()))
             criteria.andNameLike("%" + req.getName() + "%");
 
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> info = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", info.getTotal());
+        LOG.info("总页数：{}", info.getPages());
+
 //        List<EbookResp> ebookRespList = new ArrayList<>();
 
 //        for(Ebook ebook : list) {
