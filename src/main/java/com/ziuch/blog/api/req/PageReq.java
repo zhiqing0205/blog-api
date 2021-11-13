78ccaa28617cbbef09878653198461e314b31297
Zhiqing Zhong <1775840762@qq.com>
2021-11-14 00:07:29 +0800

    集成Validation验证接口参数 并对BindExpection进行统一处理

--- src/main/java/com/ziuch/blog/api/req/PageReq.java
+++ src/main/java/com/ziuch/blog/api/req/PageReq.java
@@ -1,8 +1,15 @@
 package com.ziuch.blog.api.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public class PageReq {
+
+    @NotNull(message = "【页码】不能为空")
     private int page;
 
+    @NotNull(message = "【每页条数】不能为空")
+    @Max(value = 1000, message = "【每页条数】最大为1000")
     private int size;
 
     public int getPage() {
