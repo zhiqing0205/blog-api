622461179e6019f7d2f95d83da4955a74819a125
Zhiqing Zhong <1775840762@qq.com>
2022-01-13 23:52:00 +0800

    编写redis测试接口

--- src/main/java/com/ziuch/blog/api/controller/TestController.java
+++ src/main/java/com/ziuch/blog/api/controller/TestController.java
@@ -1,17 +1,24 @@
 package com.ziuch.blog.api.controller;
 
 import com.ziuch.blog.api.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/testApi")
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    @Resource
+    private RedisTemplate<String, String> redis;
 
     @Value("${test.hello:defaultValue}")
     private String helloWorld;
@@ -29,8 +36,21 @@ public class TestController {
         return "hello world, post: " + name;
     }
 
-//    @GetMapping("/user/list")
-//    public List<User> list() {
-//        return userService.list();
-//    }
+    @GetMapping("/redisSet/{key}/{value}")
+    public String redisSet(@PathVariable String key, @PathVariable String value){
+        redis.opsForValue().set(key, value, 3600 * 24, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success!\nkey: " + key + "\nvalue:" + value;
+    }
+
+    @GetMapping("/redisGet/{key}")
+    public String redisSet(@PathVariable String key){
+        Object object = redis.opsForValue().get(key);
+        if(ObjectUtils.isEmpty(object)) {
+            LOG.error("key: {} is empty!", key);
+            return "key: " + key + " is empty!";
+        }
+        LOG.info("key: {}, value: {}", key, object);
+        return "key: " + key + "\nvalue:" + object.toString();
+    }
 }
