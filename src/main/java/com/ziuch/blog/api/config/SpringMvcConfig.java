b8fa104f3160cd9b4e03c7bdea03aa1efdba07cc
Zhiqing Zhong <1775840762@qq.com>
2022-01-14 22:18:23 +0800

    增加并配置登录拦截器

--- src/main/java/com/ziuch/blog/api/config/SpringMvcConfig.java
+++ src/main/java/com/ziuch/blog/api/config/SpringMvcConfig.java
@@ -1,17 +1,23 @@
 package com.ziuch.blog.api.config;
 
 
+import com.ziuch.blog.api.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/user/login"
+                        , "/test/**", "/ebook/list", "/category/all", "/doc/content/**", "/doc/all/** ", "/user/logout/**"
+                        , "/doc.html/**", "/webjars/**", "/swagger-resources", "/v3/**", "/testApi/**");
+    }
 }
